1bb050da435cd9cf88292356015a923e38028af5
LugonesM <54716002+LugonesM@users.noreply.github.com>
2020-04-26 14:46:34 -0300

    Update powerFunction.py

--- powerFunction.py
+++ powerFunction.py
@@ -6,5 +6,6 @@ if __name__ == '__main__':
     n = int(raw_input())
  
     if n in range(1,20):
+    #En Python 2, range() se consideraba una función, pero en Python 3 no se considera una función, sino un tipo de datos , aunque se utiliza como si fuera una función.
      for i in range(0,n):
         print(pow(i,2))
